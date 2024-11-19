fac45f20134253aa7be91051421e2976cf6ebc59
gesslar <karahd@gmail.com>
2024-11-19 10:03:20 -0500

    updating documentation

--- library/date.lua
+++ library/date.lua
@@ -12,6 +12,17 @@ if false then -- ensure that functions do not get defined
   ---result is returned as a table of three strings. However, if the `as_string`
   ---parameter is provided, the result is returned as a single string.
   ---
+  ---@example
+  ---```lua
+  ---date.shms(6543)
+  -----"01"
+  -----"49"
+  -----"03"
+  ---
+  ---date.shms(6453, true)
+  ----- "1h 49m 3s"
+  ---```
+  ---
   ---@name shms
   ---@param seconds number - The number of seconds to convert.
   ---@param as_string boolean? - Whether to return the result as a string.
