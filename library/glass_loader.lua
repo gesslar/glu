3293c5bb55baa4655a4bc4830e7cda39962e04b7
gesslar <karahd@gmail.com>
2024-11-22 22:00:29 -0500

    docs for glass_loader

---
+++ library/glass_loader.lua
@@ -0,0 +1,20 @@
+---@meta GlassLoaderClass
+
+------------------------------------------------------------------------------
+-- GlassLoaderClass
+------------------------------------------------------------------------------
+
+if false then -- ensure that functions do not get defined
+
+  ---@class GlassLoaderClass
+
+  --- Loads a glass script from a path or url.
+  ---@name load_glass
+  ---@param opts table - The options table.
+  ---@param opts.path string - The path or url to the glass script.
+  ---@param opts.cb function - The callback function.
+  ---@param opts.execute boolean? - Whether to execute the glass script.
+  ---@return any - The result of the glass script.
+  function glass_loader.load_glass(opts) end
+
+end
